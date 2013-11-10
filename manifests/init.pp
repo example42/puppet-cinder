19a74d406f6ec567d029d9ad8ad7a16f99a0c632
Alessandro Franceschi <al@lab42.it>
2013-11-10 08:31:13 +0100

    puppet module generate example42-cinder

---
+++ manifests/init.pp
@@ -0,0 +1,150 @@
+#
+# = Class: cinder
+#
+# This class installs and manages cinder
+#
+#
+# == Parameters
+#
+# Refer to https://github.com/stdmod for official documentation
+# on the stdmod parameters used
+#
+class cinder (
+
+  $package_name              = $cinder::params::package_name,
+  $package_ensure            = 'present',
+
+  $service_name              = $cinder::params::service_name,
+  $service_ensure            = 'running',
+  $service_enable            = true,
+
+  $config_file_path          = $cinder::params::config_file_path,
+  $config_file_replace       = $cinder::params::config_file_replace,
+  $config_file_require       = 'Package[cinder]',
+  $config_file_notify        = 'Service[cinder]',
+  $config_file_source        = undef,
+  $config_file_template      = undef,
+  $config_file_content       = undef,
+  $config_file_options_hash  = undef,
+
+  $config_dir_path           = $cinder::params::config_dir_path,
+  $config_dir_source         = undef,
+  $config_dir_purge          = false,
+  $config_dir_recurse        = true,
+
+  $dependency_class          = undef,
+  $my_class                  = undef,
+
+  $monitor_class             = undef,
+  $monitor_options_hash      = { } ,
+
+  $firewall_class            = undef,
+  $firewall_options_hash     = { } ,
+
+  $scope_hash_filter         = '(uptime.*|timestamp)',
+
+  $tcp_port                  = undef,
+  $udp_port                  = undef,
+
+  ) inherits cinder::params {
+
+
+  # Class variables validation and management
+
+  validate_bool($service_enable)
+  validate_bool($config_dir_recurse)
+  validate_bool($config_dir_purge)
+  if $config_file_options_hash { validate_hash($config_file_options_hash) }
+  if $monitor_options_hash { validate_hash($monitor_options_hash) }
+  if $firewall_options_hash { validate_hash($firewall_options_hash) }
+
+  $config_file_owner          = $cinder::params::config_file_owner
+  $config_file_group          = $cinder::params::config_file_group
+  $config_file_mode           = $cinder::params::config_file_mode
+
+  $manage_config_file_content = default_content($config_file_content, $config_file_template)
+
+  $manage_config_file_notify = pickx($config_file_notify)
+
+  if $package_ensure == 'absent' {
+    $manage_service_enable = undef
+    $manage_service_ensure = stopped
+    $config_dir_ensure = absent
+    $config_file_ensure = absent
+  } else {
+    $manage_service_enable = $service_enable
+    $manage_service_ensure = $service_ensure
+    $config_dir_ensure = directory
+    $config_file_ensure = present
+  }
+
+
+  # Resources managed
+
+  if $cinder::package_name {
+    package { $cinder::package_name:
+      ensure   => $cinder::package_ensure,
+    }
+  }
+
+  if $cinder::service_name {
+    service { $cinder::service_name:
+      ensure     => $cinder::manage_service_ensure,
+      enable     => $cinder::manage_service_enable,
+    }
+  }
+
+  if $cinder::config_file_path {
+    file { 'cinder.conf':
+      ensure  => $cinder::config_file_ensure,
+      path    => $cinder::config_file_path,
+      mode    => $cinder::config_file_mode,
+      owner   => $cinder::config_file_owner,
+      group   => $cinder::config_file_group,
+      source  => $cinder::config_file_source,
+      content => $cinder::manage_config_file_content,
+      notify  => $cinder::manage_config_file_notify,
+      require => $cinder::config_file_require,
+    }
+  }
+
+  if $cinder::config_dir_source {
+    file { 'cinder.dir':
+      ensure  => $cinder::config_dir_ensure,
+      path    => $cinder::config_dir_path,
+      source  => $cinder::config_dir_source,
+      recurse => $cinder::config_dir_recurse,
+      purge   => $cinder::config_dir_purge,
+      force   => $cinder::config_dir_purge,
+      notify  => $cinder::config_file_notify,
+      require => $cinder::config_file_require,
+    }
+  }
+
+
+  # Extra classes
+
+  if $cinder::dependency_class {
+    include $cinder::dependency_class
+  }
+
+  if $cinder::my_class {
+    include $cinder::my_class
+  }
+
+  if $cinder::monitor_class {
+    class { $cinder::monitor_class:
+      options_hash => $cinder::monitor_options_hash,
+      scope_hash   => {}, # TODO: Find a good way to inject class' scope
+    }
+  }
+
+  if $cinder::firewall_class {
+    class { $cinder::firewall_class:
+      options_hash => $cinder::firewall_options_hash,
+      scope_hash   => {},
+    }
+  }
+
+}
+
---
+++ manifests/params.pp
@@ -0,0 +1,41 @@
+# Class: cinder::params
+#
+# Defines all the variables used in the module.
+#
+class cinder::params {
+
+  $package_name = $::osfamily ? {
+    default => 'cinder',
+  }
+
+  $service_name = $::osfamily ? {
+    default => 'cinder',
+  }
+
+  $config_file_path = $::osfamily ? {
+    default => '/etc/cinder/cinder.conf',
+  }
+
+  $config_file_mode = $::osfamily ? {
+    default => '0644',
+  }
+
+  $config_file_owner = $::osfamily ? {
+    default => 'root',
+  }
+
+  $config_file_group = $::osfamily ? {
+    default => 'root',
+  }
+
+  $config_dir_path = $::osfamily ? {
+    default => '/etc/cinder',
+  }
+
+  case $::osfamily {
+    'Debian','RedHat','Amazon': { }
+    default: {
+      fail("${::operatingsystem} not supported. Review params.pp for extending support.")
+    }
+  }
+}
